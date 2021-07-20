4274785fdf5c1013a047480542c7a7a34eb6fc97
GulTion <gkv8192@gmail.com>
2021-07-20 09:51:15 +0530

    KnapScak is implemented

--- FKnapscak.h
+++ FKnapscak.h
@@ -5,6 +5,8 @@
 
 
 double maxProfitFKnapsack(double *weight, double *profit, int itemSize, int bagSize){
+    int currentWeight = 0;
+    double maxProfit = 0;
     double * perWeightProfit = (double*)malloc(itemSize*sizeof(double));
     T *table = (T*)malloc(sizeof(T));
 
@@ -19,14 +21,37 @@ double maxProfitFKnapsack(double *weight, double *profit, int itemSize, int bagS
     QuickSort(table, 0,itemSize,3,2);
 
     //PrintTable
-    for(int i=0;i<3;i++){
-        for(int j=0;j<itemSize;j++)
-            printf("%3.1f ",selector(table, i)[j]);
-        printf("\n");
-    }
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<itemSize;j++)
+    //         printf("%4.1f ",selector(table, i)[j]);
+    //     printf("\n");
+    // }
+
+    int i;
+   for(i=itemSize-1;i>=0;--i){
+       
+       currentWeight += table->first[i];
+       if(currentWeight<=bagSize){
+           maxProfit += table->second[i];
+       }else{
+           currentWeight -= table->first[i];
+           break;
+       }
+   }
+
+//    printf("%4.1f %d\n", maxProfit, currentWeight);
+   maxProfit += (bagSize - currentWeight)*table->third[i];
+   currentWeight += (bagSize - currentWeight);
+//    printf("%4.1f %d\n", maxProfit, currentWeight);
+    
+
+
+
     // while()
         
 
     free(table);
     free(perWeightProfit);
+
+    return maxProfit;
 }
--- example3.c
+++ example3.c
@@ -4,7 +4,7 @@ int main(){
     double weight[6] = {1,2,3,4,5,6};
     double profit[6] = {7,8,9,10,11,12};
     int sizeOfItems = 6;
-    int sizeOfBag = 14;
-    maxProfitFKnapsack(weight, profit, sizeOfItems, sizeOfBag);
-    return 0;   
+    int sizeOfBag = 5;
+    printf("MaxProfit: %4.3f",maxProfitFKnapsack(weight, profit, sizeOfItems, sizeOfBag));
+    return 0; 
 }
