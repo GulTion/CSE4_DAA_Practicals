502148e71b204a87a9c34eddf25021b215b2be1b
GulTion <gkv8192@gmail.com>
2021-07-30 11:25:20 +0530

    fraction

--- ExampleKnapSack.c
+++ ExampleKnapSack.c
@@ -1,10 +1,14 @@
 #include "./FKnapscak.h"
 
 int main(){
-    double weight[6] = {1,2,3,4,5,6};
-    double profit[6] = {7,8,9,10,11,12};
-    int sizeOfItems = 6;
-    int sizeOfBag = 5;
-    printf("MaxProfit: %4.3f",maxProfitFKnapsack(weight, profit, sizeOfItems, sizeOfBag));
+    double weight[6] = { 7,  8,  6,  9,  2,  5},
+    //                   0,  1,  2,  3,  4,  5
+           profit[6] = {11, 12, 13, 14, 15, 16};
+           
+    int sizeOfItems = 6,sizeOfBag = 20;
+    printf("\nMaxProfit: %4.3f",maxProfitFKnapsack( 
+        weight, profit, sizeOfItems, sizeOfBag));
     return 0; 
+    // O(nlogn) time
+    // O(n) space
 }
